14c3b95b5fd1369ab2e88f9987976c09ba2d7c7f
connie kephart <spindle.monkey@gmail.com>
2017-07-18 16:30:43 -0600

    resets form fields to defaults after successful cat add

--- starter-code/app/app.js
+++ starter-code/app/app.js
@@ -1,36 +1,46 @@
-  // Get all cats and spit out the JSON collection in the console
-  var catList= $.get('https://ga-cat-rescue.herokuapp.com/api/cats')
-    .done(function(data){
-      console.log(data);
-    var catObjectList = JSON.parse(catList.responseText);
-    console.log(catObjectList);
-    console.log(catObjectList.length);
+  // Get the list of cats currently at the rescue, and list them on the web site, 
+  // underneath the intake form
+  var catList = $.get('https://ga-cat-rescue.herokuapp.com/api/cats')
+    .done(function(data) {
+      //console.log(data);
+      var catObjectList = JSON.parse(catList.responseText);
+      //console.log(catObjectList);
+      //console.log(catObjectList.length);
 
-    for(i=0; i < catObjectList.length; i++){
-      $("#cats").append("<li>" + catObjectList[i].name + ' - <em>' + catObjectList[i].note + "</em></li>");
-    }
+      for (i = 0; i < catObjectList.length; i++) {
+        $('#cats').append('<li>' + catObjectList[i].name + ' - <em>' + catObjectList[i].note + '</em></li>');
+      }
     });
     
 
 
-    $("form").on("submit", function(event){
-      event.preventDefault();
-      var newCat = $('#cat-name').val();
-      var newCatNote = $('#cat-note').val();
-      console.log(newCat);
-      console.log(newCatNote);
-      var catObject = {};
-      catObject.name = newCat;
-      catObject.note = newCatNote;
-      console.log(catObject);
-      $.post('https://ga-cat-rescue.herokuapp.com/api/cats', JSON.stringify(catObject))
-        .done(function(data){
-          console.log(data);
-          var newCatData = JSON.parse(data);
+  $("form").on("submit", function(event) {
 
-          $("#cats").append("<li>" + newCatData.name + ' - <em>' + newCatData.note + "</em></li>");
-        });
-    });  
+    // don't let the form submit/refresh the page! we can handle this with js
+    event.preventDefault();
+
+    // get the cat's name and note from the form
+    var newCat = $('#cat-name').val();
+    var newCatNote = $('#cat-note').val();
+
+    // create the object with the new cat info so we can add it to the database
+    var catObject = {
+      name: newCat,
+      note: newCatNote,
+    };
+
+    //console.log(catObject);
+    $.post('https://ga-cat-rescue.herokuapp.com/api/cats', JSON.stringify(catObject))
+      .done(function(data) {
+        //console.log(data);
+        var newCatData = JSON.parse(data);
+        $('#cats').append('<li>' + newCatData.name + ' - <em>' + newCatData.note + '</em></li>');
+      });
+
+    // reset/clear the form fields
+    $('#cat-name').val('Mr. Kittums');
+    $('#cat-note').val('Tell us a bit about this cat');
+  });  
     
 
 
